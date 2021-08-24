c0424f93f46ffbd8f8a067c740b5a40d628c6d23
SeNaP <senap@bk.ru>
2021-08-24 21:18:08 +0700

    add domain allow

--- django/restaurant/settings.py
+++ django/restaurant/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-0o(twcpw(n(mi^!h9dpq6)q@w-#s!dv2(s_8_@m_xeud$^o2_7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['157.90.231.91', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['157.90.231.91', '127.0.0.1', 'localhost', 'respublica.pesya.ru']
 
 
 # Application definition

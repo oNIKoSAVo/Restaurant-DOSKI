2c1a6a9415033c0aaaeb3132fd700610bcfc36d4
SeNaP <senap@bk.ru>
2021-09-21 14:57:49 +0700

    fix

--- django/restaurant/lib/sms.py
+++ django/restaurant/lib/sms.py
@@ -7,4 +7,3 @@ API = "https://mcommunicator.ru/M2M/m2m_api.asmx/SendMessage"
 class Sms:
     def send(self, phone, message):
         response = requests.get(f"{API}?msid={phone}&message={message}&naming=beta.respublica.bar&login=&password={PASSWD}")
-        print(response.text())

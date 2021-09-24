8978c036f5aef39383d03814f8568566fbcd2e6b
SeNaP <senap@bk.ru>
2021-09-24 21:04:42 +0700

    add defautl ciphers

--- django/restaurant/lib/sms.py
+++ django/restaurant/lib/sms.py
@@ -6,4 +6,11 @@ API = "https://mcommunicator.ru/M2M/m2m_api.asmx/SendMessage"
 
 class Sms:
     def send(self, phone, message):
+        requests.packages.urllib3.disable_warnings()
+        requests.packages.urllib3.util.ssl_.DEFAULT_CIPHERS += ':HIGH:!DH:!aNULL'
+        try:
+            requests.packages.urllib3.contrib.pyopenssl.util.ssl_.DEFAULT_CIPHERS += ':HIGH:!DH:!aNULL'
+        except AttributeError:
+            # no pyopenssl support used / needed / available
+            pass
         response = requests.get(f"{API}?msid={phone}&message={message}&naming=beta.respublica.bar&login=&password={PASSWD}", verify=False)

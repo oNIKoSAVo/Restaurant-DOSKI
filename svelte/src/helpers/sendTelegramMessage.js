737e37946794fb7ee2a6e72f768b0353f62fbc60
Ortyommm <artemdemargaryan@gmail.com>
2021-12-01 12:03:09 +0300

    many ids

--- svelte/src/helpers/sendTelegramMessage.js
+++ svelte/src/helpers/sendTelegramMessage.js
@@ -1,8 +1,13 @@
-const sendMessageTelegramLink = 'https://api.telegram.org/bot2064596905:AAHnSrythz4Iu3zTFSqYQmW0p6PSW9rZZ0Y/sendMessage?chat_id=620304420'
+const sendMessageTelegramLink = 'https://api.telegram.org/bot2064596905:AAHnSrythz4Iu3zTFSqYQmW0p6PSW9rZZ0Y/sendMessage?chat_id='
+
+const chatIds = ['620304420', '675831', '154873092']
 
 export function sendTelegramMessage(msg){
-    fetch(
-        `${sendMessageTelegramLink}&text=${msg}`,
-        { method: "GET" }
-    );
+    for (const chatId of chatIds){
+        fetch(
+            `${sendMessageTelegramLink}${chatId}&text=${msg}`,
+            { method: "GET" }
+        );
+    }
+
 }
